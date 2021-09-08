22df836b14b71e9a05557df680a68e72e79a02d7
Denis Andrei <alexandone21@yahoo.com>
2021-09-08 11:56:49 +0300

    version 2

--- OnlineShopApp/Program.cs
+++ OnlineShopApp/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+// Version 2
 namespace OnlineShopApp
 {
     public class Program
